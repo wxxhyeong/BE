07a3dfde3d8d26746b69395fdf6cd7e00fee3dcb
wxxhyeong <wxxhyeong@gmail.com>
2024-09-20 15:29:57 +0900

    refactor: config 수정

--- src/main/java/com/be/config/SecurityInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.be.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
